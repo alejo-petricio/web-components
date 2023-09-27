1a0c245d3d71f479359f05f8e2d45a5d58ad3b55
alejo <alejoepetricio@gmail.com>
2023-09-26 21:36:29 -0300

    segundo commit

--- components/footer.js
+++ components/footer.js
@@ -6,28 +6,28 @@ function footer(el) {
     </div>
     <div class="footer__nav-caja">
         <div class="footer__nav-cont">
-            <img src="/imagenes/Vector (2).svg" class="footer__nav-img">
+            <img src="./imagenes/Vector (2).svg" class="footer__nav-img">
             <a href="./index.html" class="footer__nav-sites">Home</a>
         </div>
         <div class="footer__nav-cont">
-            <img src="/imagenes/user.svg" class="footer__nav-img">
+            <img src="./imagenes/user.svg" class="footer__nav-img">
             <a href="./servicios.html" class="footer__nav-sites">Servicios</a>
         </div>
         <div class="footer__nav-cont">
-            <img src="/imagenes/phone.svg" class="footer__nav-img">
+            <img src="./imagenes/phone.svg" class="footer__nav-img">
             <a href="./contacto.html" class="footer__nav-sites">Contacto</a>
         </div>
     </div>
 
     <div class="footer__nav-caja-pages">
             <a href="https://www.linkedin.com/in/alejo-elias-petricio-074b17239/" class="footer__pages">
-                <img src="/imagenes/Frame 24.svg" alt="" class="footer__pages-img">
+                <img src="./imagenes/Frame 24.svg" alt="" class="footer__pages-img">
             </a>
             <a href="https://github.com/alejo-petricio" class="footer__pages">
-                <img src="/imagenes/Frame 26.svg" alt="" class="footer__pages-img">
+                <img src="./imagenes/Frame 26.svg" alt="" class="footer__pages-img">
             </a>
             <a href="https://twitter.com/AlejoPetricio" class="footer__pages">
-                <img src="/imagenes/Frame 27.svg" alt="" class="footer__pages-img">
+                <img src="./imagenes/Frame 27.svg" alt="" class="footer__pages-img">
             </a>
     </div>
     <div class="footer__end">
--- components/header.js
+++ components/header.js
@@ -1,7 +1,7 @@
 function header(el) {
     const headerEl = document.createElement("div");
     headerEl.innerHTML = `<div class="header__cont">
-    <img src="/imagenes/Alejo_Petricio-removebg-preview.png" alt="" class="header__logo">
+    <img src="./imagenes/Alejo_Petricio-removebg-preview.png" alt="" class="header__logo">
     <a href="" class="header__burger-click">
         <img src="/imagenes/menu-svgrepo-com 1.png" alt="" class="header__burger">
     </a>
@@ -15,7 +15,7 @@ function header(el) {
 </div>
 <div class="header__burger-nav">
 <a href="">
-    <img src="/imagenes/Vector (1).svg" alt="" class="header__burger-nav-img">
+    <img src="./imagenes/Vector (1).svg" alt="" class="header__burger-nav-img">
 </a>
 <div class="header__burger-nav-container">
     <a href="./portfolio.html" class="header__burger-nav-link">Portfolio</a>
